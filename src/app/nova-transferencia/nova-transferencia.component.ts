44d56fb8e55d1e949bb86c137d045ea826ddcd51
Yasuhei13 <92935040+Yasuhei13@users.noreply.github.com>
2022-01-09 21:39:10 -0300

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number
+  destino: number
+
+
+  transferir( ) {
+    console.log('solicitado nova transferencia')
+    const valorEmitir =  {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos()
+  }
+limparCampos() {
+  this.valor = 0;
+  this.destino = 0;
+}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
